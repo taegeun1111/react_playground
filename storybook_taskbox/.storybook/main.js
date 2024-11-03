a26f196b70484cb6f1a62e649692916f211366ad
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2024-11-03 22:01:20 +0900

    CHORE: storybook init with template

---
+++ storybook_taskbox/.storybook/main.js
@@ -0,0 +1,20 @@
+import { dirname, join } from "path";
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  staticDirs: ["../public"],
+  addons: [
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
+  ],
+  framework: {
+    name: getAbsolutePath("@storybook/react-vite"),
+    options: {},
+  },
+};
+export default config;
+
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
+}
