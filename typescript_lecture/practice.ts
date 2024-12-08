6786263623902f718a6ed8328dc42f997eea9e0f
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2024-12-08 22:11:17 +0900

    FEAT: enum 대체 const

--- typescript_lecture/practice.ts
+++ typescript_lecture/practice.ts
@@ -2,9 +2,27 @@ type onlyTuple = readonly [number, string];
 
 // 타입(type) 정의에는 as const 사용 불가능
 // type onlyTuple2 = [number, string] as const;
-const readOnlyTuple2 = [1,'2'] as const;
-
+const readOnlyTuple2 = [1, "2"] as const;
 
 let readOnlyTuple: onlyTuple = [1, "2"];
 
+const enum EDirection {
+  UP = 1,
+  DOWN = 2,
+  LEFT = 3,
+  RIGHT = 4,
+}
 
+const ODircetion = {
+  UP: 1,
+  DOWN: 2,
+  LEFT: 3,
+  RIGHT: 4,
+} as const;
+
+type ODirection = (typeof ODircetion)[keyof typeof ODircetion];
+
+
+
+const test1:ODirection = 1
+const test2:EDirection = ODircetion.LEFT
