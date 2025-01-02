866071de415b79fa45d5c4c5557882212bb451c2
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2025-01-02 22:33:36 +0900

    FEAT: esmodule

--- typescript_lecture/jquery.ts
+++ typescript_lecture/jquery.ts
@@ -1,7 +1,10 @@
+
+
 $("p").removeClass("myClass noClass").addClass("yourClass");
+
 $(["p", "t"]).text("hello");
 
-const tag = $("ul li").addClass(function (index) {
+const tag = $("ul li").addClass('hello').addClass(function (index) {
   return "item-" + index;
 });
 
@@ -9,3 +12,12 @@ $(tag).html(function (i: number) {
   console.log(this);
   return $(this).data("name") + "입니다";
 });
+
+import * as $ from 'jquery';
+import $ = require('jquery');
+
+// tsconfig.json의 esModuleInterop: true
+import $ from 'jquery';
+
+export default $; // esmodule
+export = $; // commonjs
